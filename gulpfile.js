6dcb33b01d17ecad1fba5306830c5fa36ef03575
sunshine521Git <695187706@qq.com>
2017-06-08 23:33:00 +0800

    完善了gulpfile.js,已经与老师的版本一致了

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,51 @@
-// 引入 gulp 包
+//1、 引入 gulp 包
 var gulp = require('gulp')
 
-// 引入 gulp-webserver 包
+///2、引入 gulp-webserver 包
 var webserver = require('gulp-webserver')
 
-// 引入 gulp-webpack 包
-var webpack = require('gulp-webpack')
-
-// 引入 文件名提取 包
-var named = require('vinyl-named')
-
-// 引入 gulp-sass 包
-var sass = require('gulp-sass')
-
-//引入反向代理的 包
+//3、引入反向代理的 包
 var proxy = require('http-proxy-middleware')
 
-// 拷贝src下的html到build下
-gulp.task('copyhtml', function() {
-	gulp.src('./src/*.html')
-		.pipe(gulp.dest('./build/'))
-})
+///4、引入 gulp-webpack 包
+var webpack = require('gulp-webpack')
 
-// 启动一个webserver服务
+//5、引入 文件名提取 包
+var named = require('vinyl-named')
+
+//6、引入 gulp-sass 包
+var sass = require('gulp-sass')
+
+//7、版本号控制相关的 包
+var rev = require('gulp-rev')
+var revCollector = require('gulp-rev-collector')
+var del = require('del') //删除模块
+var gulpSequence = require('gulp-sequence') // 序列
+
+//8、gulp load 包
+var $ = require('gulp-load-plugins')()
+
+//9、环境切换配置 包
+var minimist = require('minimist') //可以获取命令行里的参数
+var gulpif = require('gulp-if') //if语句
+var knowOptions = {
+	string: 'env',
+	default: {
+		env: process.env.NODE_ENV || 'production'
+	}
+}
+var options = minimist(process.argv.slice(2), knowOptions)
+
+//任务1： 启动一个webserver服务
 gulp.task('webserver', function() {
-	url: './listmore'
-	gulp.src('./build/')
+	gulp.src('./dev/')
 	.pipe(
 		webserver({
-			host: 'localhost',
+			host: '10.9.166.115',
 			port: 8000,
 			directoryListing: {
 				enable: true,
-				path: './build'
+				path: './dev'
 			},
 			livereload: true,
 			middleware: [
@@ -48,26 +61,26 @@ gulp.task('webserver', function() {
 						'^/mock': ''
 						//浏览器看到mock就去9000域名，并且删除mock
 					}
+				}),
+				proxy('/api,', {
+					//目标地址
+					target: 'http://m.qbaobei.com/',
+					
+					changeOrigin: true,
+					//识别localhost向其他域名的跳转
+					
+					pathRwrite:{
+						'^/api':''
+						//浏览器看到api就去对应的域名，并且删除api
+					}
 				})
-//				Proxy('/api,', {
-//					//目标地址
-//					target: 'http://m.qbaobei.com/',
-//					
-//					changeOrigin: true,
-//					//识别localhost向其他域名的跳转
-//					
-//					pathRwrite:{
-//						'^/api':''
-//						//浏览器看到api就去对应的域名，并且删除api
-//					}
-//				})
 			]
 
 		})
 	)
 })
 
-// 打包js
+//任务2： 打包 js
 gulp.task('packjs', function() {
 	gulp.src([
 			'./src/script/app.js',
@@ -91,44 +104,68 @@ gulp.task('packjs', function() {
 				]
 			}
 		}))
-		.pipe(gulp.dest('./build/script'))
+		.pipe(gulpif(options.env === 'production', $.uglify())) //生产环境  压缩js
+		.pipe(gulpif(options.env === 'production', rev())) //生产环境  给js加版本号
+		.pipe(gulpif(options.env === 'production', gulp.dest('./build/script')))
+		//生产环境  将js输送到build下
+		.pipe(gulpif(options.env !== 'production', gulp.dest('./dev/script')))
+		//开发环境  将js输送到dev下
+		.pipe(gulpif(options.env === 'production', rev.manifest())) //生产环境  产生json文件
+		.pipe(gulpif(options.env === 'production', gulp.dest('./build/rev/script')))
+		//生产环境 将json文件输送到bulid下的rev下的script
 })
 
-// 打包 scss
+//任务3： 打包  scss
 gulp.task('packcss', function() {
 	gulp.src([
 			'./src/style/usage/app.scss',
 			'./src/style/usage/readyBaby.scss'
 		])
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./build/style'))
+		.pipe(gulpif(options.env === 'production', $.minifyCss()))
+		//生产环境  压缩css
+		.pipe(gulpif(options.env === 'production', rev()))
+		//生产环境  给css增加版本号
+		.pipe(gulpif(options.env === 'production', gulp.dest('./build/style')))
+		//生产环境  将js输送到bulid下
+		.pipe(gulpif(options.env !== 'production', gulp.dest('./dev/style')))
+		//开发环境  将js输送到dev下
+		.pipe(gulpif(options.env === 'production', rev.manifest()))
+		//生产环境  给css增加json文件
+		.pipe(gulpif(options.env === 'production', gulp.dest('./build/rev/style')))
+	//生产环境  将css的json文件输送到bulid下的rev下的style
 })
 
-// copy images
+//任务4： 拷贝src下的html到build下
+gulp.task('copyhtml', function() {
+	gulp.src('./src/*.html')
+		.pipe(gulpif(options.env === 'production', gulp.dest('./build/')))
+		.pipe(gulpif(options.env !== 'production', gulp.dest('./dev/')))
+})
+
+//任务5： html 增加版本号
+gulp.task('packhtml', function() {
+	return gulp.src(['./bulid/rev/**/*.json', './bulid/*.html'])
+		.pipe(revCollector()) //从manifests中获取静态资源版本数据, 该数据由不同的流产生, 并且替换html中的链接
+		.pipe($.minifyHtml())
+		.pipe(gulp.dest('./bulid/'))
+})
+
+//任务6： copy images
 gulp.task('copyimage', function() {
 	gulp.src('./src/images/**/*')
-		.pipe(gulp.dest('./build/images'))
+		.pipe(gulpif(options.env === 'production', gulp.dest('./build/images')))
+		.pipe(gulpif(options.env !== 'production', gulp.dest('./dev/images')))
 })
 
-// copy swiper.css
-gulp.task('copySwiperCss', function() {
-	gulp.src('./src/style/usage/module/swiper/swiper-3.4.2.min.css')
-		.pipe(gulp.dest('./build/style'))
-})
-
-// copy swiper.js
-gulp.task('copySwiperJs', function() {
-	gulp.src('./src/script/utils/swiper-3.4.2.jquery.min.js')
-		.pipe(gulp.dest('./build/script'))
-})
-
-// copy libs
+//任务7： copy libs
 gulp.task('copylibs', function() {
 	gulp.src('./src/script/libs/*.*')
-		.pipe(gulp.dest('./build/libs'))
+		.pipe(gulpif(options.env === 'production', gulp.dest('./build/libs')))
+		.pipe(gulpif(options.env !== 'production', gulp.dest('./dev/libs')))
 })
 
-// 监测文件变化
+//任务8： 监测文件变化
 gulp.task('watch', function() {
 	gulp.watch('./src/*.html', ['copyhtml'])
 	gulp.watch('./src/script/**/*.js', ['packjs'])
@@ -138,7 +175,24 @@ gulp.task('watch', function() {
 	gulp.watch('./src/libs/**/*', ['copylibs'])
 })
 
-// 定义默认任务
-gulp.task('default', ['copyhtml', 'watch', 'webserver'], function() {
-	console.log('默认任务执行完成!.')
+//任务9：删除 bulid 里的所有文件
+gulp.task('clean', del.bind(null, ['./bulid/**/*'], {
+	force: true
+}))
+
+//任务10：总的 pack任务
+gulp.task('pack', function(callback) {
+	if(options.env === 'production') {
+		//注意 串行  并行  逗号前后的有先后顺序 但是【】中间的没有先后顺序
+		gulpSequence('clean', ['packjs', 'packcss', 'copylibs', 'copyimage', 'copyhtml'], 'packhtml')(callback)
+	}else{
+		gulpSequence(['packjs', 'packcss', 'copyhtml', 'copylibs', 'copyimage'])(callback)
+	}
+
+	
+})
+
+//任务11： 定义默认任务
+gulp.task('default', ['pack', 'watch', 'webserver'], function() {
+	console.log('恭喜大神！默认任务执行完成!.')
 })
